5563070279e6cc2c786ee85abd87583b8dd14757
Ju5tAlex <justalex777@mail.ru>
2020-11-06 12:49:58 +0300

    Init commit

---
+++ src/main/java/org/example/task/cardealershop/CardealershopApplication.java
@@ -0,0 +1,13 @@
+package org.example.task.cardealershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardealershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardealershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/task/cardealershop/CardealershopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.task.cardealershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardealershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
