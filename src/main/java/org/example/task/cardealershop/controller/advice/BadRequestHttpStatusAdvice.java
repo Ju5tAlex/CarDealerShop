9036c37641f371e3f3cf9b21aab677011022bd21
Ju5tAlex <justalex777@mail.ru>
2020-11-18 17:48:55 +0300

    Reworked process of interaction with MQ Added custom exception logging

--- src/main/java/org/example/task/cardealershop/controller/advice/BadRequestHttpStatusAdvice.java
+++ src/main/java/org/example/task/cardealershop/controller/advice/BadRequestHttpStatusAdvice.java
@@ -1,6 +1,9 @@
 package org.example.task.cardealershop.controller.advice;
 
 import org.example.task.cardealershop.exception.DuplicatedListEntityException;
+import org.example.task.cardealershop.exception.NoMessagesInQueueException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,11 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class DuplicatedListEntityAdvice {
+public class BadRequestHttpStatusAdvice {
+    private Logger logger = LoggerFactory.getLogger(BadRequestHttpStatusAdvice.class);
+
     @ResponseBody
-    @ExceptionHandler(DuplicatedListEntityException.class)
+    @ExceptionHandler(value = {DuplicatedListEntityException.class, NoMessagesInQueueException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String DuplicatedEntityHandler(DuplicatedListEntityException ex) {
+    public String BadRequestHandler(RuntimeException ex) {
+        logger.error(String.format("Exception %s occurred with message \"%s\"", ex.getClass().getName(), ex.getMessage()));
         return ex.getMessage();
     }
 }
--- src/main/java/org/example/task/cardealershop/service/MQServiceImpl.java
+++ src/main/java/org/example/task/cardealershop/service/MQServiceImpl.java
@@ -1,60 +1,40 @@
 package org.example.task.cardealershop.service;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.GetResponse;
+import org.example.task.cardealershop.exception.NoMessagesInQueueException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
 
 @Service
 public class MQServiceImpl implements MQService{
 
     private Logger logger = LoggerFactory.getLogger(PartServiceImpl.class);
-    private final static String QUEUE_NAME = "PartQueue";
-    private ConnectionFactory factory;
+    private final static String QUEUE_NAME = "MyQueue";
+    private AmqpTemplate amqpTemplate;
 
-    public MQServiceImpl() {
-        factory = new ConnectionFactory();
-        factory.setHost("localhost");
+    @Autowired
+    public MQServiceImpl(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
     }
 
     @Override
     public boolean sendMessageToMQ(String message) {
-        try (Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        } catch (TimeoutException | IOException e) {
-            e.printStackTrace();
-        }
-        logger.info("Message sent ot MQ: " + message);
+        amqpTemplate.convertAndSend(QUEUE_NAME, message);
+        logger.info("Message sent to MQ: " + message);
         return true;
     }
 
     @Override
     public String receiveMessage() {
-        String message = null;
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            GetResponse response = channel.basicGet(QUEUE_NAME, false);
-            if (response == null) {
-                message = "No messages in queue to receive";
-            } else {
-                message = new String(response.getBody(), StandardCharsets.UTF_8);
-                long deliveryTag = response.getEnvelope().getDeliveryTag();
-                channel.basicAck(deliveryTag, false);
-            }
-        } catch (TimeoutException | IOException e) {
-            e.printStackTrace();
-        }
-        logger.info("Message from MQ received: " + message);
-        return message;
+        Message message = amqpTemplate.receive(QUEUE_NAME);
+        if (message == null) throw new NoMessagesInQueueException(QUEUE_NAME);
+        String messageFromQueue = new String(message.getBody(), StandardCharsets.UTF_8);
+        logger.info("Message from MQ received: " + messageFromQueue);
+        return messageFromQueue;
     }
 }
